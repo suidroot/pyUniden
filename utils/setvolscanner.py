87788dd01869ea988c2c0d5fa282f26269ee7e76
Ben Mason <locutus@the-collective.net>
2018-10-30 22:35:56 +0000

    Moving misc scripts to utils

---
+++ utils/setvolscanner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/which python3
+# Mute Uniden BCT15X Scanner 
+#
+# Author: Ben Mason
+# Version: 0.1
+#
+import sys
+import unidenbct15x
+
+__author__ = "Ben Mason"
+__copyright__ = "Copyright 2017"
+__version__ = "0.1"
+__email__ = "locutus@the-collective.net"
+__status__ = "Development"
+
+PORT = '/dev/ttyACM0'
+SPEED = 115200
+
+setvol = sys.argv[1]
+
+scanner = unidenbct15x.Unidenbct15x()
+scanner.openserial(PORT, SPEED)
+print ('Setting Volume to {0}'.format(setvol))
+scanner.volume(setvol)
+scanner.closeserial()
+
